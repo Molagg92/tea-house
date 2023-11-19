486e9426f0b1ccb7d764a8e92db15d3f53df3e7c
Erik Z <molagg92@gmail.com>
2023-11-18 16:19:12 -0800

    make the empty skeleton

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import ProductControl from "./ProductControl";
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+      <ProductControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
